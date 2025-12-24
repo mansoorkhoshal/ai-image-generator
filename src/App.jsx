847020a567c2f6ae748cfff1006a2791c7a3c1a9
Mansoor Ahmad <mansoorahmad.dev44@gmail.com>
2025-12-24 19:16:44 +0500

    installing the required packages

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+
+import { logo } from "./assets/logo"
+import { Home, CreatePost } from './pages'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+        <img src={logo} alt="Logo" />
+      </header>
+    </BrowserRouter>
+  )
+}
+
+export default App
